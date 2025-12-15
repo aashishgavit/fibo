9b33aa560527fe3dea7a39d581eb8fd5de4b98e9
aashishgavit <aashishgavit29@gmail.com>
2025-12-15 22:39:05 +0530

    BalancedBrackets

---
+++ src/fibo/BalancedBrackets.java
@@ -0,0 +1,29 @@
+package fibo;
+
+import java.util.Stack;
+
+public class BalancedBrackets {
+    public static void main(String[] args) {
+        String s = "{[()]}";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[')
+                stack.push(c);
+            else {
+                if (stack.isEmpty()) {
+                    System.out.println("Not Balanced");
+                    return;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    System.out.println("Not Balanced");
+                    return;
+                }
+            }
+        }
+        System.out.println(stack.isEmpty() ? "Balanced" : "Not Balanced");
+    }
+}
